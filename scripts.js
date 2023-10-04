47686f793a828dca6de96b2a79f3d18baf12a13b
1ea^es <121741548+MrBZBZ@users.noreply.github.com>
2023-10-04 20:44:48 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@ window.addEventListener("load", function() {
     var loader = document.getElementById("loader");
     loader.style.display = "none";
 });
+window.onload = function() {
+    var mainElements = document.querySelectorAll('main');
+    mainElements.forEach(function(element) {
+        element.style.opacity = '1';  // 设置透明度为1，使其渐显
+    });
+}
+window.addEventListener('scroll', function() {
+    var mainElements = document.querySelectorAll('main');
+
+    mainElements.forEach(function(element) {
+        var positionFromTop = element.getBoundingClientRect().top;  // 获取元素距离视口顶部的距离
+
+        if (positionFromTop - window.innerHeight <= 0) { // 当元素到达或接近视口的底部
+            element.style.transform = 'translateY(0)'; // 重置Y轴的位置
+            element.style.opacity = '1'; // 设置透明度为1
+        }
+    });
+});
